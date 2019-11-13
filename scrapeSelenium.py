31f25f2e50ae47c6cb7b50f0a13b35d9e4803b8b
Dishant Patel <tug02945@temple.edu>
2019-11-12 21:46:20 -0500

    researched best way to scrape data and also added some useful information

--- scrapeSelenium.py
+++ scrapeSelenium.py
@@ -18,15 +18,16 @@ def get_page(webPage):
     
     # get landing page of category
     driver.get(webPage)
-    
     # wait for javascript
     res = driver.execute_script("return document.documentElement.outerHTML")
-
+    courseNames[] = driver.find_elements_by_class_name('section-details-link')
+    courseTimes[] = driver.find_elements_by_class_name('meeting')
+    # print("course name is "+ courseNames)
     print("Obtained " + webPage)
     return res
 
 if __name__ == "__main__":
-    webPage = "https://www.amazon.com"
+    webPage = "https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/classSearch/classSearch"
     page = get_page(webPage)
 
-    print(page)
+    # print(page)
