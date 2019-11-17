1286702421df953a809b1e37886b2cf03537fa4b
Dishant Patel <tug02945@temple.edu>
2019-11-16 21:55:51 -0500

    able to run beautifulsoup and send it to a file

--- scrapeSelenium.py
+++ scrapeSelenium.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 
 # Finds all categories respective apps
 def get_page(webPage):
+    f = open("scraped.txt","a")
     print("Obtaining " + webPage + "...")
 
     # create a chrome instance
@@ -31,9 +32,13 @@ def get_page(webPage):
     time.sleep(10)
     for x in range(1010):
         driver.find_element_by_xpath('//*[@id="searchResultsTable"]/div[2]/div/button[3]').click()
+        html = driver.page_source
+        soup = bs.BeautifulSoup(html,'lxml')
+        f.write(str(soup))
         time.sleep(5)
     # print("course name is "+ courseNames)
     print("Obtained " + webPage)
+    f.close()
     return res
 
 if __name__ == "__main__":
