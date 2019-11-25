788de0d7e4c24f0e5ab70346d6002e6afba995de
Dishant Patel <tug02945@temple.edu>
2019-11-25 18:04:27 -0500

    created path for home and schedule page

--- flask_scheduler/app.py
+++ flask_scheduler/app.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+    return '<h1>Home Page</h1>'
+
+@app.route('/schedule')
+def output_page():
+    return '<h1>Schedule Page</h1>'
 
 if __name__ == '__main__':
     app.run()
