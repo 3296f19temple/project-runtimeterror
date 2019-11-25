66151bbbbf1e31c8883a83fa23602d6bddd88509
Dishant Patel <tug02945@temple.edu>
2019-11-25 18:16:38 -0500

    created templates for home and schedule pages

--- flask_scheduler/app.py
+++ flask_scheduler/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html')
 
 @app.route('/schedule')
 def output_page():
