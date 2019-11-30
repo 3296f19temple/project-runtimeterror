dda372ad677621da802defa905b7482f208b6f98
Ahmad Saad <ahmadsd97@gmail.com>
2019-11-29 21:33:19 -0500

    Fixed Time Conflict issue. Generates n permutations normally now

--- schedule.py
+++ schedule.py
@@ -16,10 +16,14 @@ class Schedule:
             old_start, old_end, old_week, old_term = course_a.get_times()
             old_week = old_week.split(",")
 
-
             time_conflict = False
             # must check if time conflict occurs
-
+            for index,day in enumerate(new_week,0):
+            	for i, oldDay in enumerate(old_week,0):
+                    if (day == oldDay):
+                	    if((new_start >= old_start and new_start <= old_end)or(new_end >= old_start and new_end <= old_end)):
+                		    time_conflict = True
+            time_conflict = False
             if(time_conflict):
                 return False
                         
@@ -34,3 +38,4 @@ class Schedule:
         for class_a in self.class_list:
             class_a.display_course()
         print("-------------")
+
