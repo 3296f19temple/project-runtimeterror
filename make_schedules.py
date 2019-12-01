062550bac7212fd372adae860f8a1af77589d4dc
Ahmad Saad <ahmadsd97@gmail.com>
2019-12-01 11:40:05 -0500

    Added a class to it

--- make_schedules.py
+++ make_schedules.py
@@ -7,58 +7,57 @@ from course import Course
 from schedule import Schedule
 import itertools
 import random
+class makeSchedule:
+    def get_classes(classes, x):
+        l = [random.randint(0,len(classes)-1) for i in range(x)]
 
-def get_classes(classes, x):
-    l = [random.randint(0,len(classes)-1) for i in range(x)]
+        des_class = []
+        for index, key in enumerate(classes):
+            if index in l:
+                des_class.append(key)
+                l.remove(index)
 
-    des_class = []
-    for index, key in enumerate(classes):
-        if index in l:
-            des_class.append(key)
-            l.remove(index)
+        return des_class
 
-    return des_class
+    def get_des(classes, courses_desired):
+        temp = []
+        for key in courses_desired:
+            temp.append(classes[key])
+        
+        return temp
 
-def get_des(classes, courses_desired):
-    temp = []
-    for key in courses_desired:
-        temp.append(classes[key])
-    
-    return temp
+    def get_permutations(des):
+        return list(itertools.product(*des))
 
-def get_permutations(des):
-    return list(itertools.product(*des))
+    def create_schedule:
+        classes = {}
+        with open("course_map.txt", "rb") as logFile:
+            classes = pickle.load(logFile)
 
-if __name__ == "__main__":
-    classes = {}
-    with open("course_map.txt", "rb") as logFile:
-        classes = pickle.load(logFile)
+        courses_desired = get_classes(classes, 5)
 
-    courses_desired = get_classes(classes, 5)
+        print(courses_desired)
+        print(len(courses_desired))
+        print()
+        
+        des = get_des(classes, courses_desired)
 
-    print(courses_desired)
-    print(len(courses_desired))
-    print()
-    
-    des = get_des(classes, courses_desired)
-
-    course_perm = get_permutations(des)
+        course_perm = get_permutations(des)
 
 
-    schedules = []
+        schedules = []
 
-    for schedule in course_perm:
-        isValid = True
-        sched = Schedule()
+        for schedule in course_perm:
+            isValid = True
+            sched = Schedule()
 
-        for class_a in schedule:
-            class_a.display_course()
+            for class_a in schedule:
+                class_a.display_course()
 
-            # must still check if time conflict occurs
-            isValid = sched.add_class(class_a)
-            if(not isValid):
-                break
-            
-        if(isValid):
-            schedules.append(sched)
+                # must still check if time conflict occurs
+                isValid = sched.add_class(class_a)
+                if(not isValid):
+                    break
                 
+            if(isValid):
+                schedules.append(sched)
